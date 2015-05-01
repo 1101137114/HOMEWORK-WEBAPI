13916f397b8cad2a1ef63aa68d1848d73a478137
yohsuan10126 <yohsuan10126@gmail.com>
2015-05-01 17:35:41 +0800

    new

---
+++ MvcApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using KuasCore.Services.Impl;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace MvcApplication1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        //
+        // GET: /Employee/
+
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+
+        [HttpGet]
+        [ActionName("byname")]
+        public IList<Employee> GetEmployeeByName(string name)
+        {
+            var employee = EmployeeService.GetEmployeeByName(name);
+
+            if (employee == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return employee;
+        }
+    }
+}
---
+++ MyWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using KuasCore.Models;
+using KuasCore.Services;
+using KuasCore.Services.Impl;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Http;
+
+namespace MyWebApp.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEmployeeService EmployeeService { get; set; }
+
+        [HttpGet]
+        public IList<Employee> GetAllEmployees()
+        {
+            return EmployeeService.GetAllEmployees();
+        }
+    }
+}
